53d06b29ca0ffeb094d79441b4dd1b88985f6d3a
wishka <roucke@mail.ru>
2020-06-08 00:24:32 +0300

    some change

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :admin_user
-  belongs_to :orders
+
   validates :title, presence: true
   validates :discribe, presence: true
   validates :path_to_image, presence: true
