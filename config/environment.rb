b0ec84264ee1a477d78a45efa803edd392305e46
wishka <roucke@mail.ru>
2020-06-17 15:40:31 +0300

    env change 2

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['postmaster@sandbox1246206e8d924932899ba353168ea9e7.mailgun.org'],
-  :password => ENV['87b0dea4f3d3d6d3d36514cb3fb678ed-1b6eb03d-5ff68913'],
-  :domain => 'sandbox1246206e8d924932899ba353168ea9e7.mailgun.org',
-  :address => 'smtp.mailgun.org',
-  :port => 587,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'ldc-shop.heroku.com',
   :authentication => :plain,
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
