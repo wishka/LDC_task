860f512e3dc025fec0822e9daa505494cec24029
wishka <roucke@mail.ru>
2020-06-17 14:49:19 +0300

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'ldc-shop.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
@@ -70,7 +73,7 @@ Rails.application.configure do
   :domain         => 'ldc-shop.heroku.com',
   :authentication => :plain,
   }
-  ActionMailer::Base.delivery_method = :smtp
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
