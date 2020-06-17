11b10231109086cffe38c2bf18ca1a00e4b52d49
wishka <roucke@mail.ru>
2020-06-17 15:14:52 +0300

    env change

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,10 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['app173824993@heroku.com'],
-  :password => ENV['j8tx6xhs9963'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
+  :user_name => ENV['postmaster@sandbox1246206e8d924932899ba353168ea9e7.mailgun.org'],
+  :password => ENV['87b0dea4f3d3d6d3d36514cb3fb678ed-1b6eb03d-5ff68913'],
+  :domain => 'sandbox1246206e8d924932899ba353168ea9e7.mailgun.org',
+  :address => 'smtp.mailgun.org',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
