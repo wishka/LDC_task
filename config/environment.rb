1d2cc1dd790f82b8259eec2717683f384b937a81
wishka <roucke@mail.ru>
2020-06-23 12:25:19 +0300

    v 1

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :port           => 587,
-  :address        => smtp.sendgrid.net,
-  :user_name      => apikey,
-  :password       => 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,
+  :address        => 'smtp.sendgrid.net',
+  :user_name      => 'apikey',
+  :password       => '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',
   :domain         => 'ldc-shop-new.heroku.com',
   :authentication => :plain,
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
-    :address        => smtp.sendgrid.net,
-    :user_name      => apikey,
-    :password       => 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,
-  :domain         => 'ldc-shop-new.heroku.com',
-  :authentication => :plain,
+    :address        => 'smtp.sendgrid.net',
+    :user_name      => 'apikey',
+    :password       => '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',
+  :domain           => 'ldc-shop-new.heroku.com',
+  :authentication   => :plain,
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
