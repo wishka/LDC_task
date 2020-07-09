c3253bbd204af1256a423f7120d7e4d3758adb2c
wishka <roucke@mail.ru>
2020-07-09 12:41:38 +0300

    1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to  :orders
-  belongs_to  :cart
+
   validates   :title, presence: true
   validates   :discribe, presence: true
   validates   :path_to_image, presence: true
